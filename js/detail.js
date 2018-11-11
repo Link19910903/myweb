1966006c805d20bb4bfa242b10ec59e8183b8474
liuzhaoyang <489636140@qq.com>
2018-11-11 14:00:33 +0800

    领鲜金融最终版

--- js/detail.js
+++ js/detail.js
@@ -1,8 +1,45 @@
-$('#telBuyer').on('tap',function(){
-	var buyernum = $('.buyernum').text()
-	window.location.href='tel:'+buyernum+'';
-})
-$('#telCon').on('tap',function(){
-	var connum = $('.connum').text()
-	window.location.href='tel:'+connum+'';
-})
+// $('#telBuyer').on('tap',function(){
+// 	var buyernum = $('.buyernum').text()
+// 	window.location.href='tel:'+buyernum+'';
+// })
+// $('#telCon').on('tap',function(){
+// 	var connum = $('.connum').text()
+// 	window.location.href='tel:'+connum+'';
+// })
+// $('#telBuyer').on('tap',function(){
+// 	var buyernum = $('.buyernum').text()
+// 	window.location.href='tel:'+buyernum+'';
+// })
+// $('#telCon').on('tap',function(){
+// 	var connum = $('.connum').text()
+// 	window.location.href='tel:'+connum+'';
+// })
+//mui初始化
+//买家电话
+var buyernum = $('.buyernum').text()
+mui.init({
+	swipeBack: true //启用右滑关闭功能
+});
+document.getElementById("confirmBtn").addEventListener('tap', function() {
+	var btnArray = ['取消', '立即拨打'];
+	mui.confirm(buyernum, '确定拨打电话', btnArray, function(e) {
+		if (e.index == 1) {
+			window.location.href='tel:'+buyernum+'';
+		} else {
+		}
+	})
+});
+//联系卖家
+var connum = $('.connum').text()
+mui.init({
+	swipeBack: true //启用右滑关闭功能
+});
+document.getElementById("telCon").addEventListener('tap', function() {
+	var btnArray = ['取消', '立即拨打'];
+	mui.confirm(connum, '确定拨打电话', btnArray, function(e) {
+		if (e.index == 1) {
+			window.location.href='tel:'+connum+'';
+		} else {
+		}
+	})
+});
--- js/myorder.js
+++ js/myorder.js
@@ -1,7 +1,27 @@
+var state = $('#state').text()
 $(function(){
   //获取验证码
   //确定按钮点击事件
   $('.item').on('click',function(e){
-  	window.location.href='details.html'
+  	// window.location.href='details.html'
+  	// var a =$("span[id='state']")
+  	var item = $(this).children()[0];
+  	var $item = $(item)
+  	var sta = $(item).children()[0];
+  	var $sta = $(sta)
+  	var thisState = $(sta).children()[1];
+  	var $thisState = $(thisState);
+  	var thisSta = $(thisState).text()
+  	if (thisSta == '待发货') {
+  		window.location.href='details.html'
+  	}else if (thisSta == '已发货') {
+  		window.location.href='already.html'
+  	}else if (thisSta == '订单完成') {
+  		window.location.href='fulfil.html'
+  	}else if (thisSta == '已完成') {
+  		window.location.href='fulfil.html'
+  	}else if(thisSta == '订单关闭') {
+  		window.location.href='cancel.html'
+  	}
   });
 })
